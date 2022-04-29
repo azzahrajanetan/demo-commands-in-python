b54965f73db5848d1b032405d57bf92d7028ee89
azzahrajanetan <azzahradurra@gmail.com>
2022-04-29 15:26:47 +0800

    updating code

---
+++ code/git_adding_files.py
@@ -0,0 +1,14 @@
+import os
+
+file1 = open('list-of-files.txt', 'r')
+every_item = file1.readlines()
+
+for each_item in every_item:
+    os.system(f"touch {each_item.strip()}")
+
+file2 = open('commands.txt', 'r')
+every_line = file2.readlines()
+
+for each_line in every_line:
+    os.system(f"{each_line.strip()}")
+
